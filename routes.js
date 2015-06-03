89eca46e21b8c5dfd701b877baa180da746cbc35
Pavel Matviienko <shturman.p@gmail.com>
2015-06-03 17:00:46 +0300

    initial

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 module.exports.setup = function (app, handlers) {
-  app.get('/v1/entities', handlers.entities.list);
-  app.get('/v1/entities/:id', handlers.entities.get);
-  app.post('/v1/entities', handlers.entities.create);
-  app.put('/v1/entities/:id', handlers.entities.update);
-  app.delete('/v1/entities/:id', handlers.entities.remove);
+    app.get('/', function(request, responce){
+        responce.redirect('/add_entity.html');
+    });
+    app.get('/v1/entities', handlers.entities.list);
+    app.get('/v1/entities/:id', handlers.entities.get);
+    app.post('/v1/entities', handlers.entities.create);
+    app.put('/v1/entities/:id', handlers.entities.update);
+    app.delete('/v1/entities/:id', handlers.entities.remove);
 };
